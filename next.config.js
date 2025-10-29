c6dcff904aecd49ac73ee23fa544d74684569cfc
AdamaDjan2024 <adamadjandiallo9999@gmail.com>
2025-10-29 12:18:11 +0000

    fix: Clean up merge conflicts in config files

--- next.config.js
+++ next.config.js
@@ -1,23 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   reactStrictMode: true,
-  images: {
-    domains: ["vercel.com"],
-    unoptimized: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-=======
   output: 'export',
   images: {
-    unoptimized: true,
+    domains: ["vercel.com", "mon-portfolio-zeta-bice.vercel.app"],
+    unoptimized: true
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  trailingSlash: true
->>>>>>> 6466b2e07bf75485326f2b10c988bc6a7c649937
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 module.exports = nextConfig;
