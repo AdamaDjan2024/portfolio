a123d75c2045ff591513eb7767bbe7ba73e391de
AdamaDjan2024 <adamadjandiallo9999@gmail.com>
2025-10-29 11:52:15 +0000

    Fix package.json & next.config.js (remove merge markers)

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   reactStrictMode: true,
   images: {
-    domains: ["vercel.com"],
-    unoptimized: false,
+    unoptimized: true
   },
   eslint: {
-    ignoreDuringBuilds: true,
-  },
-=======
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  trailingSlash: true
->>>>>>> 6466b2e07bf75485326f2b10c988bc6a7c649937
+    ignoreDuringBuilds: true
+  }
 };
 
 module.exports = nextConfig;
